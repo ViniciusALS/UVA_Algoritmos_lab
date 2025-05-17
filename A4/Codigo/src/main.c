b45d53fb22d519852f3a18c0d3070d68b4a8dc40
Vinicius A L Souza <contact@viniciusals.com>
2025-05-17 18:43:54 -0300

    Atualiza codigos da A4 e Projeto de Extensao

--- A4/Codigo/src/main.c
+++ A4/Codigo/src/main.c
@@ -10,38 +10,6 @@
 #endif
 
 
-#define TAMANHO_STRING 50
-
-FILE* abreOuCriaArquivo();
-void moveParaInicioDoArquivo(FILE* arquivo);
-void moveParaFimDoArquivo(FILE* arquivo);
-void sistemaLoja(FILE* lojaBD);
-OpcoesMenu exibeMenu();
-void iniciaNovoDiaDeVenda(Venda listaDeVendas[]);
-
-
-typedef struct venda {
-    char nomeCliente[TAMANHO_STRING];
-    int diaDaVenda;
-    int mesDaVenda;
-    int anoDaVenda;
-    char codigoItem[TAMANHO_STRING];
-    char nomeItem[TAMANHO_STRING];
-    char marcaItem[TAMANHO_STRING];
-    int quantidadeItem;
-    float precoUnitarioItem;
-} Venda;
-
-typedef enum opcoesMenu {
-    SAIR,
-    INICIAR_DIA
-} OpcoesMenu;
-
-typedef enum opcoesRegistro {
-    VOLTAR,
-    CONTINUAR
-} OpcoesRegistro;
-
 
 
 int main() {
@@ -55,98 +23,6 @@ int main() {
         return -1;
     }
 
-    sistemaLoja(arquivo);
     
     return 0;
-}
-
-
-FILE* abreOuCriaArquivo() {
-    return fopen("loja_roupa.txt", "a+"); 
-}
-
-// void moveParaInicioDoArquivo(FILE* arquivo) {
-//     rewind(arquivo);
-// }
-
-// void moveParaFimDoArquivo(FILE* arquivo) {
-//     fseek(arquivo, 0, SEEK_END);
-// }
-
-void sistemaLoja(FILE* lojaBD) {
-
-    Venda listaDeVendas[50];
-
-    while(1) {    
-        OpcoesMenu opcao = exibeMenu();
-
-        switch (opcao) {
-            case INICIAR_DIA:
-                iniciaNovoDiaDeVenda(listaDeVendas);
-                break;
-            
-            case SAIR:
-            default:
-                return;
-        }
-    }
-}
-
-OpcoesMenu exibeMenu() {
-
-    OpcoesMenu opcao;
-
-    clrscr();
-
-    printf("Loja de roupas Veiga de Almeida\n\n\n");
-
-    printf("Escolha o que você deseja fazer: \n\n");
-
-    printf("1 - Iniciar dia de venda \n");
-    printf("0 - Sair do programa \n\n");
-
-    printf("Opção desejada: ");
-    scanf(" %d", &opcao);
-
-    return opcao;
-}
-
-void iniciaNovoDiaDeVenda(Venda listaDeVendas[]) {
-
-    int dia, mes, ano;
-    
-    clrscr();
-
-    printf("Entre com a data do dia (dd/mm/aaaa): ");
-    scanf(" %d/%d/%d", &dia, &mes, &ano);
-
-    clrscr();
-
-    while(1) {
-
-        OpcoesRegistro opcao;
-
-        printf("Escolha uma opcão: \n\n");
-
-        printf("1 - Registrar novo cliente. \n");
-        printf("0 - Terminar dia de venda. \n\n");
-
-        printf("Opção desejada: ");
-        scanf(" %d", &opcao);
-
-        if (opcao == VOLTAR)
-            return;
-
-        while(1) {
-            char nomeCliente[50];
-
-            clrscr();
-
-            printf("Nome do cliente: ");
-            scanf("%s", nomeCliente);
-
-            
-        }
-
-    }
 }
